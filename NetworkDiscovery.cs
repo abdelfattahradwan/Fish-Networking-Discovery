5310d38fa68e07d4b913875aca97ab07b71ea574
Abdelfattah Radwan <ARadwan620@gmail.com>
2022-01-17 19:36:23 +0200

    Initial commit

---
+++ NetworkDiscovery.cs
@@ -0,0 +1,179 @@
+﻿using FishNet.Managing;
+using FishNet.Managing.Logging;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FishNet.Discovery
+{
+	public sealed class NetworkDiscovery : MonoBehaviour
+	{
+		[SerializeField]
+		private string secret;
+
+		[SerializeField]
+		private ushort port;
+
+		[SerializeField]
+		private float discoveryInterval;
+
+		private UdpClient _serverUdpClient;
+		private UdpClient _clientUdpClient;
+
+		public event Action<IPEndPoint> ServerFoundCallback;
+
+		private void OnDisable()
+		{
+			StopAdvertisingServer();
+
+			StopSearchingForServers();
+		}
+
+		private void OnDestroy()
+		{
+			StopAdvertisingServer();
+
+			StopSearchingForServers();
+		}
+
+		private void OnApplicationQuit()
+		{
+			StopAdvertisingServer();
+
+			StopSearchingForServers();
+		}
+
+		#region Server
+
+		public void StartAdvertisingServer()
+		{
+			if (!InstanceFinder.IsServer)
+			{
+				if (NetworkManager.StaticCanLog(LoggingType.Warning)) Debug.LogWarning("Unable to start advertising server. Server is inactive.", this);
+
+				return;
+			}
+
+			if (_serverUdpClient != null)
+			{
+				if (NetworkManager.StaticCanLog(LoggingType.Common)) Debug.Log("Server is already being advertised.", this);
+
+				return;
+			}
+
+			_serverUdpClient = new UdpClient(port)
+			{
+				EnableBroadcast = true,
+				MulticastLoopback = false,
+			};
+
+			Task.Run(AdvertiseServerAsync);
+
+			if (NetworkManager.StaticCanLog(LoggingType.Common)) Debug.Log("Started advertising server.", this);
+		}
+
+		public void StopAdvertisingServer()
+		{
+			if (_serverUdpClient == null) return;
+
+			_serverUdpClient.Close();
+
+			_serverUdpClient = null;
+
+			if (NetworkManager.StaticCanLog(LoggingType.Common)) Debug.Log("Stopped advertising server.", this);
+		}
+
+		private async void AdvertiseServerAsync()
+		{
+			while (_serverUdpClient != null)
+			{
+				await Task.Delay(TimeSpan.FromSeconds(discoveryInterval));
+
+				var result = await _serverUdpClient.ReceiveAsync();
+
+				var receivedSecret = Encoding.UTF8.GetString(result.Buffer);
+
+				if (receivedSecret == secret)
+				{
+					var okBytes = BitConverter.GetBytes(true);
+
+					await _serverUdpClient.SendAsync(okBytes, okBytes.Length, result.RemoteEndPoint);
+				}
+			}
+		}
+
+		#endregion
+
+		#region Client
+
+		public void StartSearchingForServers()
+		{
+			if (InstanceFinder.IsServer)
+			{
+				if (NetworkManager.StaticCanLog(LoggingType.Warning)) Debug.LogWarning("Unable to start searching for servers. Server is active.", this);
+
+				return;
+			}
+
+			if (InstanceFinder.IsClient)
+			{
+				if (NetworkManager.StaticCanLog(LoggingType.Warning)) Debug.LogWarning("Unable to start searching for servers. Client is active.", this);
+
+				return;
+			}
+
+			if (_clientUdpClient != null)
+			{
+				if (NetworkManager.StaticCanLog(LoggingType.Common)) Debug.Log("Already searching for servers.", this);
+
+				return;
+			}
+
+			_clientUdpClient = new UdpClient()
+			{
+				EnableBroadcast = true,
+				MulticastLoopback = false,
+			};
+
+			Task.Run(SearchForServersAsync);
+
+			if (NetworkManager.StaticCanLog(LoggingType.Common)) Debug.Log("Started searching for servers.", this);
+		}
+
+		public void StopSearchingForServers()
+		{
+			if (_clientUdpClient == null) return;
+
+			_clientUdpClient.Close();
+
+			_clientUdpClient = null;
+
+			if (NetworkManager.StaticCanLog(LoggingType.Common)) Debug.Log("Stopped searching for servers.", this);
+		}
+
+		private async void SearchForServersAsync()
+		{
+			var secretBytes = Encoding.UTF8.GetBytes(secret);
+
+			var endPoint = new IPEndPoint(IPAddress.Broadcast, port);
+
+			while (_clientUdpClient != null)
+			{
+				await Task.Delay(TimeSpan.FromSeconds(discoveryInterval));
+
+				await _clientUdpClient.SendAsync(secretBytes, secretBytes.Length, endPoint);
+
+				var result = await _clientUdpClient.ReceiveAsync();
+
+				var response = BitConverter.ToBoolean(result.Buffer);
+
+				if (response) ServerFoundCallback?.Invoke(result.RemoteEndPoint);
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ NetworkDiscoveryHUD.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+namespace FishNet.Discovery
+{
+	public sealed class NetworkDiscoveryHUD : MonoBehaviour
+	{
+		[SerializeField]
+		private NetworkDiscovery networkDiscovery;
+
+		private readonly List<IPEndPoint> _endPoints = new();
+
+		private Vector2 _serversListScrollVector;
+
+		private void Start()
+		{
+			if (networkDiscovery == null) networkDiscovery = FindObjectOfType<NetworkDiscovery>();
+
+			networkDiscovery.ServerFoundCallback += (endPoint) =>
+			{
+				if (!_endPoints.Contains(endPoint)) _endPoints.Add(endPoint);
+			};
+		}
+
+		private void OnGUI()
+		{
+			var buttonHeight = GUILayout.Height(35.0f);
+
+			using (new GUILayout.AreaScope(new Rect(Screen.width - 240.0f - 10.0f, 10.0f, 240.0f, Screen.height - 20.0f)))
+			{
+				GUILayout.Box("Server");
+
+				using (new GUILayout.HorizontalScope())
+				{
+					if (GUILayout.Button("Start", buttonHeight)) InstanceFinder.ServerManager.StartConnection();
+
+					if (GUILayout.Button("Stop", buttonHeight)) InstanceFinder.ServerManager.StopConnection(true);
+				}
+
+				GUILayout.Box("Advertising");
+
+				using (new GUILayout.HorizontalScope())
+				{
+					if (GUILayout.Button("Start", buttonHeight)) networkDiscovery.StartAdvertisingServer();
+
+					if (GUILayout.Button("Stop", buttonHeight)) networkDiscovery.StopAdvertisingServer();
+				}
+
+				GUILayout.Box("Searching");
+
+				using (new GUILayout.HorizontalScope())
+				{
+					if (GUILayout.Button("Start", buttonHeight)) networkDiscovery.StartSearchingForServers();
+
+					if (GUILayout.Button("Stop", buttonHeight)) networkDiscovery.StopSearchingForServers();
+				}
+
+				if (_endPoints.Count > 0)
+				{
+					GUILayout.Box("Servers");
+
+					using (new GUILayout.ScrollViewScope(_serversListScrollVector))
+					{
+						for (var i = 0; i < _endPoints.Count; i++)
+						{
+							var ipAddress = _endPoints[i].Address.ToString();
+
+							if (GUILayout.Button(ipAddress))
+							{
+								networkDiscovery.StopAdvertisingServer();
+
+								networkDiscovery.StopSearchingForServers();
+
+								InstanceFinder.ClientManager.StartConnection(ipAddress);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
