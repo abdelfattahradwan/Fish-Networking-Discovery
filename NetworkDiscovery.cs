e598fc5f0891f92fdbb3985c2d40f2addc5a9e9c
Abdelfattah Radwan <ARadwan620@gmail.com>
2023-08-21 22:46:18 +0300

    Revert "Merge pull request #3 from emredesu/main"
    This reverts commit 389387d99bc10cf085d72740d96986e80412bce9, reversing
    changes made to d489a58c48941784178debc3857a52c243c24702.

--- NetworkDiscovery.cs
+++ NetworkDiscovery.cs
@@ -2,7 +2,6 @@
 using FishNet.Managing.Logging;
 using FishNet.Transporting;
 using System;
-using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -71,16 +70,6 @@ namespace FishNet.Discovery
 		/// </summary>
 		public event Action<IPEndPoint> ServerFoundCallback;
 
-		/// <summary>
-		/// This list stores all the found <see cref="IPEndPoint"/>s that have been discovered by the worker thread to be processed by the main thread in the Update() function.
-		/// </summary>
-		private List<IPEndPoint> foundIPEndPoints = new();
-
-		/// <summary>
-		/// This list stores all the previously found <see cref="IPEndPoint"/>s, so that they are not considered as "found" again when a new network discovery is run.
-		/// </summary>
-		private List<IPEndPoint> previouslyFoundIPEndPoints = new();
-
 		private void Start()
 		{
 			if (automatic)
@@ -90,36 +79,6 @@ namespace FishNet.Discovery
 				InstanceFinder.ClientManager.OnClientConnectionState += ClientConnectionStateChangedHandler;
 
 				StartSearchingForServers();
-			}
-		}
-
-		private void Update() {
-			// See if we have found any IPEndPoints, and if we have, invoke the ServerFoundCallback event.
-			if (foundIPEndPoints.Count > 0) {
-				// Invoke the ServerFoundCallback with every IP address in the list.
-				foreach (IPEndPoint foundIPEndPoint in foundIPEndPoints) {
-					// Compare the address of the found endpoint with every previously found endpoint. If it matches any, do not invoke the ServerFoundCallback for it.
-					bool previouslyFound = false;
-
-					foreach (IPEndPoint previouslyFoundEndPoint in previouslyFoundIPEndPoints) {
-						if (previouslyFoundEndPoint.Address.ToString() == foundIPEndPoint.Address.ToString()) {
-                            previouslyFound = true;
-							break;
-						}
-					}
-
-					if (previouslyFound) {
-						continue;
-					}
-					else {
-						ServerFoundCallback?.Invoke(foundIPEndPoint);
-
-						previouslyFoundIPEndPoints.Add(foundIPEndPoint);
-					}
-				}
-
-				// Now clear all the IP addresses.
-				foundIPEndPoints.Clear();
 			}
 		}
 
@@ -294,8 +253,6 @@ namespace FishNet.Discovery
 				return;
 			}
 
-			previouslyFoundIPEndPoints.Clear();
-
 			_clientUdpClient = new UdpClient()
 			{
 				EnableBroadcast = true,
@@ -313,8 +270,6 @@ namespace FishNet.Discovery
 		public void StopSearchingForServers()
 		{
 			if (_clientUdpClient == null) return;
-
-			previouslyFoundIPEndPoints.Clear();
 
 			_clientUdpClient.Close();
 
@@ -339,9 +294,9 @@ namespace FishNet.Discovery
 
 				if (BitConverter.ToBoolean(result.Buffer, 0))
 				{
-					if (!foundIPEndPoints.Contains(result.RemoteEndPoint)) {
-						foundIPEndPoints.Add(result.RemoteEndPoint);
-					}
+					ServerFoundCallback?.Invoke(result.RemoteEndPoint);
+
+					StopSearchingForServers();
 				}
 			}
 		}
